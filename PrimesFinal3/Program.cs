773963ab269c9a15787742860daa0ddc20accae1
Dom R <ramutd@outlook.com>
2021-09-14 11:17:19 -0400

    Added my primes generator

---
+++ PrimesFinal3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrimesFinal3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                
+            }
+        }
+    }
+}
